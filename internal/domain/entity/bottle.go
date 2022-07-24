6bc14d018f5316646206738e00fab465ac7e3401
Alejandro Iglesias <ikalejandro94@gmail.com>
2022-07-24 18:27:52 -0300

    Add bottling sqlc repo

--- internal/domain/entity/bottle.go
+++ internal/domain/entity/bottle.go
@@ -3,3 +3,19 @@ package entity
 type Bottle struct {
 	baseEntity
 }
+
+func NewBottle(id string, name string) (*Bottle, error) {
+	return &Bottle{baseEntity{uuid: id, name: name}}, nil
+}
+
+func (b Bottle) ID() string {
+	return b.uuid
+}
+
+func (b Bottle) Name() string {
+	return b.name
+}
+
+func UnmarshalBottleFromDatabase(id string, name string) (*Bottle, error) {
+	return NewBottle(id, name)
+}
--- internal/domain/repository/bottle_repository.go
+++ internal/domain/repository/bottle_repository.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/alejandroik/trazavino/internal/domain/entity"
+)
+
+type BottleRepository interface {
+	AddBottle(ctx context.Context, bottle *entity.Bottle) error
+	GetBottle(ctx context.Context, bottleId string) (*entity.Bottle, error)
+	ListBottles(ctx context.Context, offset int32, limit int32) ([]*entity.Bottle, error)
+	UpdateBottle(
+		ctx context.Context,
+		bottleId string,
+		updateFn func(ctx context.Context, b *entity.Bottle) (*entity.Bottle, error),
+	) error
+}
